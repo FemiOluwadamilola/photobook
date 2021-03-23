75f21c9395026539e3b1d701b0ea1c0b4051d0d8
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2021-03-23 02:09:20 +0100

    done with home page and login UI

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const ejs = require('ejs');
+const dotenv = require('dotenv');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const mongo = require('mongodb');
+// const Routes = require('./routes/index');
 
 const app = express();
+// load config
+dotenv.config({path:'./config/config.env'});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+// EJS
+app.use(expressLayouts);
+app.set('layout','./layouts/layout')
+app.set('view engine','ejs');
+
+// BODY PASSER AND PUBLIC FOLDER
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+// ROUTER MIDDLEWARE
+app.use('/', require('./routes/route'));
+
+// ENV VARIABLE
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`app running in ${process.env.NODE_ENV} mode on ${PORT}`));
